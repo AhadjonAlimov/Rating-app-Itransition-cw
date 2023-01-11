45427a23f9193335ac01d611fe2bcad2e669a6f3
Alimov <Akhadjonalimov@gmail.com>
2023-01-11 16:21:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+const path = require("path");
 const fileUpload = require("express-fileupload");
 const { notFound } = require("./plugins/helpers");
 const dotenv = require("dotenv");
